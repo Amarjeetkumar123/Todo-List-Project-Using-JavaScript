c48b73322cbb71af233480de2460ae982feee345
Amarjeet Kumar <amarjeetkumar802212@gmail.com>
2022-12-10 18:12:41 +0530

    adding Enter key pressed functionality to add todo

--- TodoList/app.js
+++ TodoList/app.js
@@ -25,3 +25,30 @@ btn.addEventListener("click", () => {
   list.append(li);
   inp.value = "";
 });
+
+
+inp.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
+    const TodoText = inp.value;
+
+    if (TodoText === "") {
+      alert("Empty Todo...");
+      return;
+    }
+
+    const li = document.createElement("li");
+    li.innerHTML = `${TodoText} <i class="fa-solid fa-trash-can"></i>`;
+
+    // adding remove todo event
+    li.querySelector("i").addEventListener("click", () => {
+      li.remove();
+    });
+
+    li.addEventListener("click", () => {
+      li.classList.toggle("done");
+    });
+
+    list.append(li);
+    inp.value = "";
+  }
+});
